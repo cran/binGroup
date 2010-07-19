883a6e100936fc3cf275991a11b051e149670d11
Frank Schaarschmidt <schaarschmidt@biostat.uni-hannover.de>
2010-07-19 00:00:00 +0000

    version 1.0-6

--- R/binCI.R
+++ R/binCI.R
@@ -1,3 +1,93 @@
+"binAC" <-
+function(n, y, conf.level=0.95, alternative="two.sided")
+{
+alpha=1-conf.level
+est=y/n
+z1s=qnorm(conf.level)
+z2s=qnorm(1-alpha/2)
+
+esti1s=(y+(z1s^2)/2)/(n+z1s^2)
+esti2s=(y+(z2s^2)/2)/(n+z2s^2)
+
+ni1s=n+z1s^2
+ni2s=n+z2s^2
+
+if(alternative=="two.sided"){
+
+CI=c(esti2s-z2s*sqrt(esti2s*(1-esti2s)/(ni2s)),
+     esti2s+z2s*sqrt(esti2s*(1-esti2s)/(ni2s)) )
+}
+else{if (alternative=="less"){
+CI=c( 0 , esti1s+z1s*sqrt(esti1s*(1-esti1s)/(ni1s)) )
+}
+
+else{if(alternative=="greater"){
+CI=c(esti1s-z1s*sqrt(esti1s*(1-esti1s)/(ni1s)), 1 )
+}
+else {stop("alternative mis-specified")}}}
+
+CI
+}
+
+"binBlaker" <-
+function (n,y,conf.level=0.95, tolerance=1e-04, alternative="two.sided")
+{
+acceptbin <- function(y,n,p)
+{
+  p1 = 1-pbinom(y-1, n, p)
+  p2 = pbinom(y, n, p)
+  a1 = p1 + pbinom( qbinom(p1,n,p)-1, n, p )
+  a2 = p2+1-pbinom( qbinom(1-p2,n,p), n, p )
+  return(min(a1,a2))
+}
+
+lower<-0
+upper<-1
+
+if(y!=0)
+  {lower<-qbeta((1-conf.level)/2, y, n-y+1)
+    {while(acceptbin(y,n,lower+tolerance)<(1-conf.level))
+    lower=lower+tolerance}
+  }
+
+if(y!=n)
+  {upper<-qbeta(1-(1-conf.level)/2, y+1, n-y)
+    {while(acceptbin(y,n,upper-tolerance)<(1-conf.level))
+    upper=upper-tolerance}
+  }
+
+c(lower, upper)
+
+}
+
+"binCP" <-
+function(n, y, conf.level=0.95, alternative="two.sided")
+{
+lower<-0
+upper<-1
+if(alternative=="two.sided")
+{
+ if(y!=0)
+   {lower<-qbeta((1-conf.level)/2, y, n-y+1)}
+ if(y!=n)
+   {upper<-qbeta(1-(1-conf.level)/2, y+1, n-y)}
+}
+if(alternative=="less")
+{
+ if(y!=n)
+   {upper<-qbeta(1-(1-conf.level), y+1, n-y)}
+}
+if(alternative=="greater")
+{
+ if(y!=0)
+   {lower<-qbeta((1-conf.level), y, n-y+1)}
+}
+
+c(lower,upper)
+
+}
+
+
 "binCI" <-
 function(n, y, conf.level=0.95, alternative="two.sided", method="CP")
 
@@ -41,4 +131,84 @@ out<-list(conf.int=conf.int,
 class(out)<-"binCI"
 out
 }
+
+
+"binSOC" <-
+function(n,y,conf.level=0.95,alternative="two.sided")
+
+{
+esti<-y/n
+kappa<-qnorm(conf.level)
+eta<-(kappa^2)/3 + 1/6
+gamma1<-((13/18)*kappa^2 + 17/18)*(-1)
+gamma2<-(kappa^2)/18 + 7/36
+
+midpo<-(y+eta)/(n+2*eta)
+
+if(alternative=="less")
+  {CI=c( 0 , midpo + kappa * sqrt(esti*(1-esti) + (gamma1*esti*(1-esti) + gamma2)/n)/sqrt(n) )}
+
+if(alternative=="greater")
+  {CI=c( midpo - kappa*sqrt(esti*(1-esti) + (gamma1*esti*(1-esti) + gamma2)/n)/sqrt(n) , 1)}
+
+if (alternative=="two.sided")
+{
+kappa<-qnorm(1-(1-conf.level)/2)
+eta<-(kappa^2)/3 + 1/6
+gamma1<-((13/18)*kappa^2 + 17/18)*(-1)
+gamma2<-(kappa^2)/18 + 7/36
+
+CI=c( midpo - kappa*sqrt(esti*(1-esti) + (gamma1*esti*(1-esti) + gamma2)/n)/sqrt(n) , 
+ midpo + kappa*sqrt(esti*(1-esti) + (gamma1*esti*(1-esti) + gamma2)/n)/sqrt(n) )
+}
+CI
+}
+
+"binWald" <-
+function(n, y, conf.level=0.95, alternative="two.sided")
+{
+alpha=1-conf.level
+est=y/n
+z1s=qnorm(conf.level)
+z2s=qnorm(1-alpha/2)
+
+if(alternative=="two.sided"){
+KI=c(est-z2s*sqrt(est*(1-est)/(n)),
+     est+z2s*sqrt(est*(1-est)/(n)) )
+}
+else{if (alternative=="less"){
+KI=c( 0 , est+z1s*sqrt(est*(1-est)/(n)) )
+}
+else{if(alternative=="greater"){
+KI=c(est-z1s*sqrt(est*(1-est)/(n)), 1 )
+}
+else {stop("alternative mis-specified")}}}
+KI
+}
+
+"binWilson" <-
+function(n,y,conf.level=0.95,alternative="two.sided") {
+alpha=1-conf.level
+t=y/n
+if(alternative =="two.sided"){
+	est.int=(y+(qnorm(1-alpha/2)^2)/2)/(n+(qnorm(1-alpha/2))^2)
+	w.se=((qnorm(1-alpha/2))*sqrt(n*t*(1-t)+(qnorm(1-alpha/2)^2)/4))/(n+qnorm(1-alpha/2)^2)
+	CI=c( est.int-w.se, est.int+w.se )
+}
+else{if(alternative=="less"){
+	est.int=(y+(qnorm(1-alpha)^2)/2)/(n+(qnorm(1-alpha))^2)
+	w.se=((qnorm(1-alpha))*sqrt(n*t*(1-t)+(qnorm(1-alpha)^2)/4))/(n+qnorm(1-alpha)^2)
+	CI=c( 0, est.int+w.se )
+} 
+else{if(alternative=="greater"){
+	est.int=(y+(qnorm(1-alpha)^2)/2)/(n+(qnorm(1-alpha))^2)
+	w.se=((qnorm(1-alpha))*sqrt(n*t*(1-t)+(qnorm(1-alpha)^2)/4))/(n+qnorm(1-alpha)^2)
+	CI=c( est.int-w.se , 1 )
+}
+else{stop("argument alternative misspecified")}}}
+
+CI
+
+}
+
 
